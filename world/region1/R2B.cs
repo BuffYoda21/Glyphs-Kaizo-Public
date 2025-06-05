7b8d3487d736ee2b7dfbacf8bfa7508284962aad
BuffYoda21 <buffyoda21@gmail.com>
2025-06-05 15:00:39 -0500

    Update R2B
    Reworked logic to have the dash as the collectable item instead of the sword and added a dash button to the room so the player can progress.

--- world/region1/R2B.cs
+++ world/region1/R2B.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using MelonLoader;
+using Il2Cpp;
 
 namespace GlyphsKaizo.World.Region1 {
     public class R2B
@@ -12,15 +13,21 @@ namespace GlyphsKaizo.World.Region1 {
                 MelonLogger.Error("R2B Room Reference not found!");
                 return;
             }
-            sword = Object.Instantiate(worldManager.sword, roomReference.transform);
-            sword.transform.localPosition = new Vector3(-4.5f, 2.8f, 0f);
-            //todo: Delete door on right, move button to the right and turn into green button,
-            //      possibly instead turn original button into blue button and clone it and move
-            //      the clone to the right and turn it into a green button.
-            //      Might also need to add a platform to the right so players can get back up without dash.
+            dashOrb = Object.Instantiate(worldManager.dashOrb, roomReference.transform);
+            dashOrb.transform.localPosition = new Vector3(-4.5f, 2.8f, 0f);
+            dashOrb.SetActive(true);
+            roomReference.transform.Find("Tiles/Door (5)").gameObject.SetActive(false);
+            GameObject button2 = Object.Instantiate(roomReference.transform.Find("Button (1)").gameObject, roomReference.transform);
+            button2.name = "Button (2)";
+            button2.transform.localPosition = new Vector3(-10.75f, -2.25f, 0f);
+            button2.transform.localRotation = Quaternion.Euler(0f, 0f, 270f);
+            button2.transform.Find("Button").GetComponent<ButtonObj>().type = "dash";
+            button2.transform.Find("Button").GetComponent<SpriteRenderer>().color = new Color(0f, 0.6059f, 1f, 1f);
+            //need to add logic and a custom save system to handle the dash orb
+            //wait nvm maybe it just works? more testing needed
         }
 
         private static GameObject roomReference;
-        private static GameObject sword;
+        private static GameObject dashOrb;
     }
 }
