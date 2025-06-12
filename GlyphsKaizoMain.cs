57c61b7b843bfd2d108b31ad5f95b2412c6d8950
BuffYoda21 <buffyoda21@gmail.com>
2025-06-12 16:07:50 -0500

    Update version number
    v1.0.0-Dev.7.0 -> v1.0.0-Dev.7.1

--- GlyphsKaizoMain.cs
+++ GlyphsKaizoMain.cs
@@ -5,7 +5,7 @@ using GlyphsKaizo.World;
 using GlyphsKaizo.Bosses.Spearman;
 using GlyphsKaizo.defense;
 
-[assembly: MelonInfo(typeof(GlyphsKaizo.Main), "Glyphs Kaizo", "1.0.0-Dev.7.0", "BuffYoda21")]
+[assembly: MelonInfo(typeof(GlyphsKaizo.Main), "Glyphs Kaizo", "1.0.0-Dev.7.1", "BuffYoda21")]
 [assembly: MelonGame("Vortex Bros.", "GLYPHS")]
 
 namespace GlyphsKaizo {
