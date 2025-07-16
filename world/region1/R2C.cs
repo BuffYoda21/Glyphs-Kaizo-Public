d073ce579b190034304082bbb023c0b86966cf18
BuffYoda21 <buffyoda21@gmail.com>
2025-07-16 15:14:15 -0500

    cloned enemy spawn
    bugged right now

--- world/region1/R2C.cs
+++ world/region1/R2C.cs
@@ -1,20 +1,21 @@
 using MelonLoader;
 using UnityEngine;
+using GlyphsKaizo.Scripts;
+using Il2CppSystem.Collections.Generic;
 
 namespace GlyphsKaizo.World.Region1
 {
-    public class R2C
-    {
-        public static void Load(GameObject regionReference)
-        {
+    public class R2C {
+        public static void Load(GameObject regionReference) {
             roomReference = regionReference.transform.Find("(R2C)").gameObject;
-            if (roomReference == null)
-            {
+            if (!roomReference) {
                 MelonLogger.Error("R0B Room Reference not found!");
                 return;
             }
             roomReference.transform.Find("Tiles/FlowerHide").gameObject.SetActive(false);
+            enemies = Utils.Pattern(roomReference.transform.Find("FloorEnemy")?.gameObject, Utils.axis.X, 3, 2);
         }
         public static GameObject roomReference;
+        public static List<GameObject> enemies;
     }
 }
