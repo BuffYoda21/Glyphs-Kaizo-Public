95f7584968d490382b843f05421491999a4aba9c
BuffYoda21 <buffyoda21@gmail.com>
2025-07-16 14:14:06 -0500

    fixed an issue where cloned GameObjects despawn immediately

--- scripts/utils.cs
+++ scripts/utils.cs
@@ -1,14 +1,17 @@
+using Il2CppSystem.Collections.Generic;
 using UnityEngine;
 
 namespace GlyphsKaizo.Scripts {
     public static class Utils {
         // copies the GameObject passed in the number of times specified in a linear pattern
-        public static void Pattern(GameObject org, axis a, int instances, int spacing, int rotation) {
-            if (!org || instances <= 1) return;
+        public static List<GameObject> Pattern(GameObject org, axis a, int instances, int spacing, int rotation) {
+            if (!org || instances <= 1) return null;
+            List<GameObject> output = new List<GameObject>();
+            output.Add(org);
             for (int i = 1; i < instances; i++) {
                 switch (a) {
                     case axis.X:
-                        Object.Instantiate(
+                        output.Add(Object.Instantiate(
                             org,
                             new Vector3(
                                 org.transform.localPosition.x + spacing,
@@ -21,10 +24,10 @@ namespace GlyphsKaizo.Scripts {
                                 org.transform.localRotation.z + rotation
                             ),
                             org.transform.parent
-                        );
-                    break;
+                        ));
+                        break;
                     case axis.Y:
-                        Object.Instantiate(
+                        output.Add(Object.Instantiate(
                             org,
                             new Vector3(
                                 org.transform.localPosition.x,
@@ -37,14 +40,15 @@ namespace GlyphsKaizo.Scripts {
                                 org.transform.localRotation.z + rotation
                             ),
                             org.transform.parent
-                        );
-                    break;
+                        ));
+                        break;
                 }
             }
+            return output;
         }
 
-        public static void Pattern(GameObject org, axis a, int instances, int spacing) {
-            Pattern(org, a, instances, spacing, 0);
+        public static List<GameObject> Pattern(GameObject org, axis a, int instances, int spacing) {
+            return Pattern(org, a, instances, spacing, 0);
         }
 
         public enum axis {
