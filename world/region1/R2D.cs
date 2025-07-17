1fcb0d55459f3ff460967fb6d32bf3ef7b48ce34
BuffYoda21 <buffyoda21@gmail.com>
2025-07-17 13:35:28 -0500

    moved enemy logic to R2D.cs
    this is because the enemy in question is part of R2D not R2C

--- world/region1/R2D.cs
+++ world/region1/R2D.cs
@@ -1,17 +1,20 @@
 using MelonLoader;
 using UnityEngine;
+using GlyphsKaizo.Scripts;
 using Il2CppSystem.Collections.Generic;
 
 namespace GlyphsKaizo.World.Region1
 {
-    public class R2C {
+    public class R2D {
         public static void Load(GameObject regionReference) {
-            roomReference = regionReference.transform.Find("(R2C)").gameObject;
+            roomReference = regionReference.transform.Find("(R2D)").gameObject;
             if (!roomReference) {
                 MelonLogger.Error("R0B Room Reference not found!");
                 return;
             }
-            roomReference.transform.Find("Tiles/FlowerHide").gameObject.SetActive(false);
+            GameObject e = roomReference.transform.Find("FloorEnemy").gameObject;
+            if(e) e.transform.localPosition = new Vector3(e.transform.localPosition.x - 2, e.transform.localPosition.y, e.transform.localPosition.z);
+            enemies = Utils.Pattern(e, Utils.axis.X, 3, 2);
         }
         public static GameObject roomReference;
         public static List<GameObject> enemies;
