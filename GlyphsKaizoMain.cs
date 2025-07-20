535e2f611ff49e75b81b6fac8cf5c69fae51df5d
BuffYoda21 <buffyoda21@gmail.com>
2025-07-19 20:26:07 -0500

    moved injection logic to AssetLoader.cs

--- GlyphsKaizoMain.cs
+++ GlyphsKaizoMain.cs
@@ -1,13 +1,9 @@
 using UnityEngine;
 using MelonLoader;
-using Il2CppInterop.Runtime.Injection;
 using GlyphsKaizo.World;
-using GlyphsKaizo.Bosses.Spearman;
-using GlyphsKaizo.defense;
-using GlyphsKaizo.Scripts.Puzzles;
 using GlyphsKaizo.scripts;
 
-[assembly: MelonInfo(typeof(GlyphsKaizo.Main), "Glyphs Kaizo", "1.0.0-Dev.9.2", "BuffYoda21")]
+[assembly: MelonInfo(typeof(GlyphsKaizo.Main), "Glyphs Kaizo", "1.0.0-Dev.9.3", "BuffYoda21")]
 [assembly: MelonGame("Vortex Bros.", "GLYPHS")]
 
 namespace GlyphsKaizo {
@@ -20,30 +16,16 @@ namespace GlyphsKaizo {
             }
             var harmony = new HarmonyLib.Harmony("GlyphsKaizo.Patches");
             harmony.PatchAll();
-            InjectKaizoClasses();
+            AssetLoader.init();
             GameObject worldManager = new GameObject("KaizoWorldManager");
             UnityEngine.Object.DontDestroyOnLoad(worldManager);
             worldManager.AddComponent<KaizoWorldManager>();
             //worldManager.AddComponent<TheNuke>();
             //obj.AddComponent<TheWatch>();
-            AssetLoader.init();
             MelonLogger.Msg("Glyphs Kaizo loaded successfully!");
             Debug.Log("I know you are reading this Landon lolllllll");
             isInitialized = true;
         }
-
-        private void InjectKaizoClasses() {
-            MelonLogger.Msg("Injecting Custom Kaizo Classes...");
-            ClassInjector.RegisterTypeInIl2Cpp<KaizoWorldManager>();
-            ClassInjector.RegisterTypeInIl2Cpp<KaizoSpearBoss>();                   //probably gonna rework boss loading to a master loader and inject that instead
-            ClassInjector.RegisterTypeInIl2Cpp<KaizoSpearBossCoroutineHelper>();
-            ClassInjector.RegisterTypeInIl2Cpp<KaizoFloorEnemy>();
-            ClassInjector.RegisterTypeInIl2Cpp<KaizoFlyingEnemy>();
-            //ClassInjector.RegisterTypeInIl2Cpp<TheWatch>();
-            //ClassInjector.RegisterTypeInIl2Cpp<TheNuke>();
-            ClassInjector.RegisterTypeInIl2Cpp<Frag1>();                            //probably going to move puzzle scripts into a seperate puzzle loader later
-        }
-
         bool isInitialized = false;
     }
 }
