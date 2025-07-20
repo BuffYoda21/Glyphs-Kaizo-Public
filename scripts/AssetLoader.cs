e1d6e1a462f31b01a4612489b2ece2c83fcbb6fa
BuffYoda21 <buffyoda21@gmail.com>
2025-07-19 20:26:34 -0500

    moved injection logic to AssetLoader.cs

--- scripts/AssetLoader.cs
+++ scripts/AssetLoader.cs
@@ -1,11 +1,24 @@
+using GlyphsKaizo.Bosses.Spearman;
+using GlyphsKaizo.Scripts.Puzzles;
+using GlyphsKaizo.World;
 using Il2Cpp;
+using Il2CppInterop.Runtime.Injection;
 using UnityEngine;
+
 namespace GlyphsKaizo.scripts {
     public class AssetLoader {
         public static void init() {
             assetParent = new GameObject("KaizoCustomAssets");
             UnityEngine.Object.DontDestroyOnLoad(assetParent);
             square = Resources.Load<GameObject>("prefabs/betweenrooms/Room1  _ _").transform.Find("wall").GetComponent<SpriteRenderer>().sprite;
+            ClassInjector.RegisterTypeInIl2Cpp<KaizoWorldManager>();
+            ClassInjector.RegisterTypeInIl2Cpp<KaizoSpearBoss>();                   //probably gonna rework boss loading to a master loader and inject that instead
+            ClassInjector.RegisterTypeInIl2Cpp<KaizoSpearBossCoroutineHelper>();
+            ClassInjector.RegisterTypeInIl2Cpp<KaizoFloorEnemy>();
+            ClassInjector.RegisterTypeInIl2Cpp<KaizoFlyingEnemy>();
+            //ClassInjector.RegisterTypeInIl2Cpp<TheWatch>();
+            //ClassInjector.RegisterTypeInIl2Cpp<TheNuke>();
+            ClassInjector.RegisterTypeInIl2Cpp<Frag1>();                            //probably going to move puzzle scripts into a seperate puzzle loader later
             initalized = true;
         }
 
