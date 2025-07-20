771c7b63e36ee44a0307118e9caa45b648866db7
BuffYoda21 <buffyoda21@gmail.com>
2025-07-19 23:03:26 -0500

    added laser script

---
+++ scripts/Laser.cs
@@ -0,0 +1,50 @@
+using GlyphsKaizo.Scripts;
+using Il2Cpp;
+using MelonLoader;
+using UnityEngine;
+
+namespace GlyphsKaizo.scripts.Assets {
+    [RegisterTypeInIl2Cpp]
+    public class Laser : MonoBehaviour {
+        public void Start() {
+            ab = base.GetComponent<AttackBox>();
+            sr = base.GetComponent<SpriteRenderer>();
+            bc = base.GetComponent<BoxCollider2D>();
+            camHover = GameObject.Find("Main Camera Parent")?.GetComponent<Hover>();
+            warningTimer = Time.time + warningTime;
+        }
+
+        public void Update() {
+            if (firing && Time.time > durationTimer) {
+                camHover.enabled = false;
+                Destroy(this.gameObject);
+            }
+            if (!charging && !firing) {
+                this.transform.localScale = new Vector3(0.05f, this.transform.localScale.y, 1f);
+                this.bc.enabled = false;
+                charging = true;
+            }
+            if (charging) {
+                if (Time.time > warningTimer) {
+                    durationTimer = Time.time + duration;
+                    this.transform.localScale = new Vector3(1f, this.transform.localScale.y, 1f);
+                    camHover.enabled = true;
+                    bc.enabled = true;
+                    firing = true;
+                    charging = false;
+                }
+            }
+        }
+
+        public Hover camHover;
+        public bool charging = false;
+        public bool firing = false;
+        public float duration = 1f;
+        public float warningTime = 1.5f;
+        public float durationTimer = 0f;
+        public float warningTimer = 0f;
+        public AttackBox ab;
+        public SpriteRenderer sr;
+        public BoxCollider2D bc;
+    }
+}
