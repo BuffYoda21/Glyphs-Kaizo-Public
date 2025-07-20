72658307c7c853425907693e2e4073ec789b1c3a
BuffYoda21 <buffyoda21@gmail.com>
2025-07-19 20:21:15 -0500

    moved injection logic to AssetLoader.cs

---
+++ AssetLoader.cs
@@ -0,0 +1,47 @@
+using Il2Cpp;
+using UnityEngine;
+namespace GlyphsKaizo.scripts {
+    public class AssetLoader {
+        public static void init() {
+            assetParent = new GameObject("KaizoCustomAssets");
+            UnityEngine.Object.DontDestroyOnLoad(assetParent);
+            square = Resources.Load<GameObject>("prefabs/betweenrooms/Room1  _ _").transform.Find("wall").GetComponent<SpriteRenderer>().sprite;
+            initalized = true;
+        }
+
+        public static GameObject Load(string name) {
+            if (!initalized) init();
+            switch (name) {
+                case "BigLaser": return LoadBigLaser();
+            }
+            return null;
+        }
+
+        private static GameObject LoadBigLaser() {
+            Transform existing = assetParent.transform.Find("BigLaser");
+            if (existing != null) {
+                return existing.gameObject; // Return the existing template
+            }
+            GameObject laser = new GameObject("BigLaser");
+            laser.transform.SetParent(assetParent.transform);
+            laser.SetActive(false);
+            laser.transform.localScale = new Vector3(1f, 200f, 1f);
+            BoxCollider2D bc = laser.AddComponent<BoxCollider2D>();
+            bc.size = new Vector2(1f, 1f);
+            bc.isTrigger = true;
+            AttackBox ab = laser.AddComponent<AttackBox>();
+            ab.attackType = "enemy";
+            ab.damage = 35;
+            ab.multihit = true;
+            SpriteRenderer sr = laser.AddComponent<SpriteRenderer>();
+            sr.sprite = square;
+            sr.size = new Vector2(1f, 1f);
+            sr.color = new Color(1f, 0f, 0f, .6f);
+            return laser;
+        }
+
+        public static GameObject assetParent;
+        public static bool initalized = false;
+        public static Sprite square;
+    }
+}
