6f07f23b8ddebabda504a6501b639158bf9f7f57
BuffYoda21 <buffyoda21@gmail.com>
2025-07-21 10:58:36 -0500

    added funni explosion

--- enemies/BossLoader.cs
+++ enemies/BossLoader.cs
@@ -9,36 +9,62 @@ namespace GlyphsKaizo.enemies {
         public void Update() {
             Scene scene = SceneManager.GetActiveScene();
             if (scene.name != "Game" && scene.name != "Memory") return;
-            MonoBehaviour bossAI = DetectBoss();
-            switch (bossAI) {
-                case DashBoss dashBoss:
-                    if (dashBoss.gameObject.GetComponent<KaizoDashBoss>()) return;
-                    KaizoDashBoss kaizoDashBoss = dashBoss.gameObject.AddComponent<KaizoDashBoss>();
-                    kaizoDashBoss.orbparent = dashBoss.orbparent;
-                    GameObject orb1 = kaizoDashBoss.orbparent.transform.Find("AttackOrb").gameObject;
-                    GameObject orb2 = kaizoDashBoss.orbparent.transform.Find("AttackOrb (1)").gameObject;
-                    orb1.AddComponent<KaizoAttackOrb>();
-                    DestroyImmediate(orb1.GetComponent<DashBossAttackOrb>());
-                    orb2.AddComponent<KaizoAttackOrb>();
-                    DestroyImmediate(orb2.GetComponent<DashBossAttackOrb>());
-                    orb1.GetComponent<KaizoAttackOrb>().init();
-                    orb2.GetComponent<KaizoAttackOrb>().init();
-                    kaizoDashBoss.init();
-                    kaizoDashBoss.orbs = new KaizoAttackOrb[2];
-                    kaizoDashBoss.orbs[0] = orb1.GetComponent<KaizoAttackOrb>();
-                    kaizoDashBoss.orbs[1] = orb2.GetComponent<KaizoAttackOrb>();
-                    kaizoDashBoss.onPhase3 = dashBoss.onPhase3;
-                    kaizoDashBoss.orbspeed = dashBoss.orbspeed;
-                    kaizoDashBoss.spike = dashBoss.spike;
-                    kaizoDashBoss.bb = dashBoss.bb;
-                    break;
+            if (!pc) pc = GameObject.Find("Player")?.GetComponent<PlayerController>();
+            if (!sm) sm = GameObject.Find("Manager Intro")?.GetComponent<SaveManager>();
+            if (explosionTimer > 0f && Time.time > explosionTimer) {
+                DestroyImmediate(explosionClone);
+                explosionTimer = 0f;
             }
-            if (bossAI) DestroyImmediate(bossAI);
+            if (!bossActive) {
+                bossAI = DetectBoss();
+                switch (bossAI) {
+                    case DashBoss dashBoss:
+                        if (dashBoss.gameObject.GetComponent<KaizoDashBoss>()) return;
+                        KaizoDashBoss kaizoDashBoss = dashBoss.gameObject.AddComponent<KaizoDashBoss>();
+                        kaizoDashBoss.orbparent = dashBoss.orbparent;
+                        GameObject orb1 = kaizoDashBoss.orbparent.transform.Find("AttackOrb").gameObject;
+                        GameObject orb2 = kaizoDashBoss.orbparent.transform.Find("AttackOrb (1)").gameObject;
+                        orb1.AddComponent<KaizoAttackOrb>();
+                        DestroyImmediate(orb1.GetComponent<DashBossAttackOrb>());
+                        orb2.AddComponent<KaizoAttackOrb>();
+                        DestroyImmediate(orb2.GetComponent<DashBossAttackOrb>());
+                        orb1.GetComponent<KaizoAttackOrb>().init();
+                        orb2.GetComponent<KaizoAttackOrb>().init();
+                        kaizoDashBoss.init();
+                        kaizoDashBoss.orbs = new KaizoAttackOrb[2];
+                        kaizoDashBoss.orbs[0] = orb1.GetComponent<KaizoAttackOrb>();
+                        kaizoDashBoss.orbs[1] = orb2.GetComponent<KaizoAttackOrb>();
+                        kaizoDashBoss.onPhase3 = dashBoss.onPhase3;
+                        kaizoDashBoss.orbspeed = dashBoss.orbspeed;
+                        kaizoDashBoss.spike = dashBoss.spike;
+                        kaizoDashBoss.bb = dashBoss.bb;
+                        DestroyImmediate(dashBoss);
+                        bossAI = kaizoDashBoss;
+                        bossActive = true;
+                        break;
+                }
+            } else {
+                if (pc.dying)
+                    bossActive = false;                             //prevents explosions when respawning
+                if (!bossAI) {
+                    explosion = Resources.Load<GameObject>("prefabs/game/explosion");
+                    explosion.SetActive(false);
+                    explosion.GetComponent<AttackBox>().attackType = "player";
+                    explosionClone = Object.Instantiate(explosion);
+                    explosionClone.transform.position = bossPos;
+                    explosionClone.SetActive(true);
+                    bossActive = false;
+                    explosionTimer = Time.time + 3f;
+                    //implement save checking logic here for first save button
+                } else {
+                    bossPos = bossAI.transform.position;
+                }
+            }
         }
 
         //Add more boss detection as needed
         private MonoBehaviour DetectBoss() {
-            MonoBehaviour bossAI = null;
+            bossAI = null;
             Scene scene = SceneManager.GetActiveScene();
             switch (scene.name) {
                 case "Game":
@@ -52,5 +78,14 @@ namespace GlyphsKaizo.enemies {
                 return bossAI;
             return null;
         }
+
+        MonoBehaviour bossAI;
+        bool bossActive = false;
+        GameObject explosion;
+        GameObject explosionClone;
+        Vector3 bossPos;
+        float explosionTimer = 0f;
+        PlayerController pc;
+        SaveManager sm;
     }
 }
